9eb49b19177f94bd129faf520c4e199eea43fa28
Moritz Eysholdt <moritz@gitpod.io>
2024-07-15 20:42:27 +0000

    fix build

--- shared/build.sh
+++ shared/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
 echo "Building Shared Code"
 echo "Building Shared Code" > shared.log
--- team-b/build.sh
+++ team-b/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
 echo "Building Team B Code"
 echo "Building Team B Code" > team-b.log
